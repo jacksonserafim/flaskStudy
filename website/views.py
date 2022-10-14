a669325fa51746816dee55c062c4a3c4718eb74a
Jackson <jacksonlserafim@gmail.com>
2022-10-14 00:56:21 -0300

    Commit #5
    * Agora é possível adicionar e remover anotações.

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
-import  json
+import json
 
 views = Blueprint('views', __name__)
 
@@ -23,9 +23,10 @@ def homepage():
 
     return render_template('home.html', user=current_user)
 
+
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
-    note = json.load(request.data)
+    note = json.loads(request.data)
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
